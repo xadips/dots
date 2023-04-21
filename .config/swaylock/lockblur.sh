d7e0709e455e07ee36c7f114959982fa96564051
xadips <29689012+xadips@users.noreply.github.com>
2023-04-21 04:52:32 +0300

    General fixes add imagemagick add initial rofi

--- .config/swaylock/lockblur.sh
+++ .config/swaylock/lockblur.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grim /tmp/lock_background.png
-convert -blur 0x1.5 /tmp/lock_background.png /tmp/lock_background_post_processed.png && rm /tmp/lock_background.png
-swaylock -f --config "$XDG_CONFIG_HOME"/swaylock/config -i /tmp/lock_background_post_processed.png
+grim -t jpeg -q 50 /tmp/lock_background.jpeg
+convert -blur 0x1.5 /tmp/lock_background.jpeg /tmp/lock_background_post_processed.jpeg && rm /tmp/lock_background.jpeg
+swaylock -f --config "$HOME"/.config/swaylock/config -i /tmp/lock_background_post_processed.jpeg
