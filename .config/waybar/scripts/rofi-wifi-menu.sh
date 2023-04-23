1582afc5902d846b48f42f542f7229686dcef42c
xadips <29689012+xadips@users.noreply.github.com>
2023-04-23 14:31:35 +0300

    Change shell script to bash

--- .config/waybar/scripts/rofi-wifi-menu.sh
+++ .config/waybar/scripts/rofi-wifi-menu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 notify-send "Getting list of available Wi-Fi networks..."
 # Get a list of available wifi connections and morph it into a nice-looking list
@@ -6,33 +6,33 @@ wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  
 
 connected=$(nmcli -fields WIFI g)
 if [[ "$connected" =~ "enabled" ]]; then
-	toggle="\ufaa9  Disable Wi-Fi"
+    toggle="\ufaa9  Disable Wi-Fi"
 elif [[ "$connected" =~ "disabled" ]]; then
-	toggle="\ufaa8  Enable Wi-Fi"
+    toggle="\ufaa8  Enable Wi-Fi"
 fi
 
 # Use rofi to select wifi network
-chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " )
+chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: ")
 # Get name of connection
 chosen_id=$(echo "${chosen_network:3}" | xargs)
 
 if [ "$chosen_network" = "" ]; then
-	exit
+    exit
 elif [ "$chosen_network" = "直  Enable Wi-Fi" ]; then
-	nmcli radio wifi on
+    nmcli radio wifi on
 elif [ "$chosen_network" = "睊  Disable Wi-Fi" ]; then
-	nmcli radio wifi off
+    nmcli radio wifi off
 else
-	# Message to show when connection is activated successfully
-	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
-	# Get saved connections
-	saved_connections=$(nmcli -g NAME connection)
-	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
-		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
-	else
-		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: " )
-		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
-	fi
+    # Message to show when connection is activated successfully
+    success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
+    # Get saved connections
+    saved_connections=$(nmcli -g NAME connection)
+    if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
+        nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
+    else
+        if [[ "$chosen_network" =~ "" ]]; then
+            wifi_password=$(rofi -dmenu -p "Password: ")
+        fi
+        nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
+    fi
 fi
