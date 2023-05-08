743f77ef48ed84a0a3d8e6e44c557080b4a9d231
xadips <29689012+xadips@users.noreply.github.com>
2023-05-08 14:38:02 +0300

    Change back to official plex patched package

--- .config/waybar/scripts/keymap.sh
+++ .config/waybar/scripts/keymap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LAYOUT=$(hyprctl devices | rg -A 2 'duckychannel-international-co.,-ltd.-ducky-keyboard-1' | rg keymap | awk '{ print $3 }')
+LAYOUT=$(hyprctl devices | rg -A 2 'duckychannel-international-co.,-ltd.-ducky-keyboard-1' | grep keymap | awk '{ print $3 }')
 [[ "$LAYOUT" == "English" ]] && echo "us" || echo "lt"
