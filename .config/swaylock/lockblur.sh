149dd9178339d2d5eda93992bcd62111e27e38c1
xadips <29689012+xadips@users.noreply.github.com>
2024-11-24 12:53:06 +0200

    updates

--- .config/swaylock/lockblur.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grim -t jpeg -q 50 /tmp/lock_background.jpeg
-convert -blur 0x1.5 /tmp/lock_background.jpeg /tmp/lock_background_post_processed.jpeg && rm /tmp/lock_background.jpeg
-swaylock -f --config "$HOME"/.config/swaylock/config -i /tmp/lock_background_post_processed.jpeg
